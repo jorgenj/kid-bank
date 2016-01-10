054f0008cbb646a3f455cd8fbce760b723f5b3ac
Jorgen Johnson <jorgen.johnson@oracle.com>
2016-01-10 09:07:54 -0800

    Add balance for accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,11 @@
 class Account < ActiveRecord::Base
   # double-entry book-keeping design based on this article:
   # http://web.archive.org/web/20131013080026/http://homepages.tcp.co.uk/~m-wigley/gc_wp_ded.html
+
+  has_many :postings
+
+  ## TODO: cache on updates to postings
+  def balance
+    postings.sum(:amount)
+  end
 end
